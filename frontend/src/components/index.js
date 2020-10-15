69f4dd3af214ff5d83498989ad5d1c04750eb0b6
Jeonghyeon Kim <powergee101@gmail.com>
2020-10-15 10:11:17 +0900

    Add a slider in Main.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,1 @@
 export { default as NavigationBar } from './NavigationBar';
-export { default as SlideViewer } from './SlideViewer';
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import { NavigationBar } from '../components'
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import slide1 from "../images/slide-sample-1.png";
+import slide2 from "../images/slide-sample-2.png";
+import './Main.scss'
 
 export default function Main() {
     return (
         <div>
             <NavigationBar></NavigationBar>
+
+            <Slide easing="ease">
+                <div className="each-slide">
+                    <div style={{ 'backgroundImage': `url(${slide1})` }}>
+                        <span>예시 슬라이드 1</span>
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div style={{ 'backgroundImage': `url(${slide2})` }}>
+                        <span>예시 슬라이드 2</span>
+                    </div>
+                </div>
+            </Slide>
+
+            
         </div>
     )
 }
