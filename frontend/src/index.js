c3dd49025483d532e6b92806155923e62862f1c7
Jeonghyeon Kim <powergee101@gmail.com>
2021-01-07 02:11:34 +0900

    Add polyfill to support IE11 (fixes #1)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,8 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
