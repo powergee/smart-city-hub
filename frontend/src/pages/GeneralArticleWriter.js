107fad87caab2dcef5905120eca19d6df32197bc
Jeonghyeon Kim <powergee101@gmail.com>
2021-01-15 03:18:20 +0900

    Add react-draft-wysiwyg

---
+++ frontend/src/pages/GeneralArticleWriter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Editor } from 'react-draft-wysiwyg';
+import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+export default function GeneralArticleWriter() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
