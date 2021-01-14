d058a48e1ff6240aed199fdbcc633b1adc15416a
Jeonghyeon Kim <powergee101@gmail.com>
2021-01-15 02:53:06 +0900

    Add a footer (fix #6)

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -24,7 +24,12 @@ export default function Main() {
                     <Route path="*" component={NotFound}></Route>
                 </Switch>
             </div>
-            {/* 여기에 푸터를 만들 예정 */}
+            <footer className="main-footer">
+                <p>서울시립대학교 국제도시과학대학원 (서울시 동대문구 서울시립대로 163, 법학관 517호)</p>
+                <p>University of Seoul, International School of Urban Sciences (Law School 517, Seoulsiripdaero 163, Dongdaemun-gu, Seoul 02504 KOREA)</p>
+                <p>Tel : +82-2-6490-5154 / CP : +82-10-2969-2504 / Email : chunhoy7@uos.ac.kr</p>
+                <strong>Copyright 2021 Global Urban &#38; Infrastructure Research Center. All Rights Reserved.</strong>
+            </footer>
         </div>
     )
 }
