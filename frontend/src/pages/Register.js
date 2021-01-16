c1ecc12dd0c696ebd7d2b0e87badfcea3d27c8d6
Jeonghyeon Kim <powergee101@gmail.com>
2021-01-17 01:33:49 +0900

    Add a password textfield in Register to make a user to retype the password

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -7,9 +7,11 @@ import "./Register.scss";
 export default function Register() {
     const [id, setId]= useState("");
     const [pw, setPw]= useState("");
+    const [pwRe, setPwRe]= useState("");
     const [name, setName]= useState("");
     const [idError, setIdError] = useState(false);
     const [pwError, setPwError] = useState(false);
+    const [pwReError, setPwReError] = useState(false);
     const [nameError, setNameError] = useState(false);
     const [buttonText, setButtonText] = useState("회원가입");
     const history = useHistory();
@@ -34,6 +36,16 @@ export default function Register() {
         setPw(value);
     }
 
+    function handlePwReChange(event) {
+        const value = event.target.value;
+        if (value !== pw) {
+            setPwReError(true);
+        } else {
+            setPwReError(false);
+        }
+        setPwRe(value);
+    }
+
     function handleNameChange(event) {
         const value = event.target.value;
         if (value.length < 1) {
@@ -49,6 +61,8 @@ export default function Register() {
             alert("ID를 입력해주세요.");
         } else if (pw.length < 6) {
             alert("비밀번호를 6자리 이상 입력해주세요.");
+        } else if (pwRe !== pw) {
+            alert("입력된 두 비밀번호가 서로 다릅니다.");
         } else if (name.length < 1) {
             alert("이름을 입력해주세요.");
         } else {
@@ -86,6 +100,10 @@ export default function Register() {
                     <TextField 
                         variant="outlined" helperText="비밀번호는 6자리 이상이어야 합니다." id="userPw" error={pwError}
                         label="비밀번호" type="password" required rows={1} onChange={handlePwChange} value={pw}/>
+
+                    <TextField 
+                        variant="outlined" helperText="비밀번호를 다시 입력해주세요." id="userPwRetype" error={pwReError}
+                        label="비밀번호 확인" type="password" required rows={1} onChange={handlePwReChange} value={pwRe}/>
 
                     <TextField 
                         variant="outlined" helperText="이름을 입력해주세요." id="userName" error={nameError}
