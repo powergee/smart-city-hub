a5b537da0aca543ec8db844d5d1cd3864ea005cf
Jeonghyeon Kim <powergee101@gmail.com>
2021-01-20 10:40:16 +0900

    Add file-saver

--- frontend/src/shared/BackendRequests.js
+++ frontend/src/shared/BackendRequests.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import fileSave from "file-saver"
 import * as crypto from 'crypto';
 
 export async function getSalt(id) {
@@ -153,20 +154,13 @@ export async function downloadFile(fileId) {
             responseType: "blob"
         });
 
-        const url = window.URL.createObjectURL(new Blob([res.data]));
-        const link = document.createElement("a");
-        link.href = url;
-
         const cd = res.headers["content-disposition"];
         const fnIdx = cd.indexOf("filename");
-        const left = cd.indexOf("\"", fnIdx)
-        const right = cd.indexOf("\"", left+1)
-        const filename = cd.substring(left+1, right)
+        const left = cd.indexOf("\"", fnIdx);
+        const right = cd.indexOf("\"", left+1);
+        const filename = cd.substring(left+1, right);
 
-        link.setAttribute("download", filename);
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+        fileSave(new Blob([res.data]), filename);
     } catch (err) {
         console.error("In downloadFile: " + err.response.data);
         throw err?.response?.status;
