b2121dc463e8f788af17cae30dd3c51d32bde37a
Jeonghyeon Kim <powergee101@gmail.com>
2021-03-02 08:50:53 +0900

    Add "커뮤니티" on NoticeBoard (Fix #19)

--- frontend/src/components/NoticeBoard.js
+++ frontend/src/components/NoticeBoard.js
@@ -6,15 +6,16 @@ import ArticlePreview from './ArticlePreview';
 import { useHistory } from 'react-router-dom';
 
 export default function NoticeBoard() {
+    // 현재 NoticeBoard 에서는 소식(news), 커뮤니티(community) 분야의 글을 다루고 있음.
     const [selected, setSelected] = useState("notices");
     const [articles, setArticles] = useState({
         "notices": [],
-        "events": [],
+        "community": [],
         "smart-news": []
     });
     const [counts, setCounts] = useState({
         "notices": 0,
-        "events": 0,
+        "community": 0,
         "smart-news": 0
     });
     const history = useHistory();
@@ -22,33 +23,51 @@ export default function NoticeBoard() {
     useEffect(() => {
         let promises = [];
         promises.push(getArticles("notices", 1, 3));
-        promises.push(getArticles("events", 1, 3));
+        promises.push(getArticles("seminar", 1, 3));
+        promises.push(getArticles("workshop", 1, 3));
+        promises.push(getArticles("readings", 1, 3));
         promises.push(getArticles("smart-news", 1, 3));
         promises.push(countArticles("notices"));
-        promises.push(countArticles("events"));
+        promises.push(countArticles("seminar"));
+        promises.push(countArticles("workshop"));
+        promises.push(countArticles("readings"));
         promises.push(countArticles("smart-news"));
 
+        // 공지사항 탭: kind가 notices인 것들에서 최근 3개 글을 표시
+        // 스마트뉴스 탭: kind가 smart-news인 것들에서 최근 3개 글을 표시
+        // 커뮤니티 탭: 커뮤니티에 포함되는 소분류는 세미나, 워크숍, 추천 도서가 있으므로,
+        //      그것들 각각에서 3개를 가져온 뒤 시간순으로 정렬하여 상위 3개를 표시
         Promise.all(promises).then((results) => {
             setArticles({
                 "notices": results[0],
-                "events": results[1],
-                "smart-news": results[2]
+                "community": results[1].concat(results[2], results[3]).sort((a, b) => {
+                    return b - a;
+                }).slice(0, 3),
+                "smart-news": results[4]
             })
 
             setCounts({
-                "notices": results[3],
-                "events": results[4],
-                "smart-news": results[5]
+                "notices": results[5],
+                "community": results[6] + results[7] + results[8],
+                "smart-news": results[9]
             })
         });
     }, [])
 
     function handleArticleClick(article) {
-        history.push("/news/" + article.kind + "/" + article.articleId);
+        if (article.kind == "notices" || article.kind == "smart-news") {
+            history.push("/news/" + article.kind + "/" + article.articleId);
+        } else {
+            history.push("/community/" + article.kind + "/" + article.articleId);
+        }
     }
 
     function moveToList() {
-        history.push("/news/" + selected);
+        if (selected == "notices" || selected == "smart-news") {
+            history.push("/news/" + selected);
+        } else {
+            history.push("/community");
+        }
     }
 
     const getButtonHandler = (kind) => () => {
@@ -60,6 +79,7 @@ export default function NoticeBoard() {
             <div className="board-header">
                 <Button onClick={getButtonHandler("notices")} color={(selected === "notices" ? "primary" : "default")} size="large">공지사항</Button>
                 <Button onClick={getButtonHandler("smart-news")} color={(selected === "smart-news" ? "primary" : "default")} size="large">스마트 뉴스</Button>
+                <Button onClick={getButtonHandler("community")} color={(selected === "community" ? "primary" : "default")} size="large">커뮤니티</Button>
             </div>
             <div className="board-contents">
                 {articles[selected].map(element => <ArticlePreview article={element} onClick={handleArticleClick}></ArticlePreview>)}
