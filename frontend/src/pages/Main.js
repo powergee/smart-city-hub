d5bfdf09ec01b053f6460c717bf74e3c475ccaa2
Jeonghyeon Kim <powergee101@gmail.com>
2021-04-06 12:16:20 +0900

    Update the address in footer

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -25,8 +25,8 @@ export default function Main() {
                 </Switch>
             </div>
             <footer className="main-footer">
-                <p>서울시립대학교 국제도시과학대학원 (서울시 동대문구 서울시립대로 163, 법학관 517호)</p>
-                <p>University of Seoul, International School of Urban Sciences (Law School 517, Seoulsiripdaero 163, Dongdaemun-gu, Seoul 02504 KOREA)</p>
+                <p>서울시립대학교 도시과학연구원 국제도시 및 인프라 연구센터 (서울특별시 동대문구 서울시립대로 163, 21세기관 104-1호)</p>
+                <p>University of Seoul, Institute of Urban Science, Global Urban &#38; Infrastructure Research Center (The 21st Century Building, Seoulsiripdaero 163, Dongdaemun-gu, Seoul 02504 KOREA)</p>
                 <p>Tel : +82-2-6490-5154 / CP : +82-10-2969-2504 / Email : chunhoy7@uos.ac.kr</p>
                 <strong>Copyright 2021 Global Urban &#38; Infrastructure Research Center. All Rights Reserved.</strong>
             </footer>
