bbeb75ed21a0ad6c50ec22b1e95eaf5b89916c49
Jeonghyeon Kim <powergee101@gmail.com>
2021-04-27 16:59:52 +0900

    Add interactive mode on backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,30 @@
 import db from "./utils/database";
 import app from "./utils/app";
 import env from "./env"
+import readline from "readline";
+import { executeLine } from "./interact";
 
 db.prepareDB();
 
-const port = env.port || 4000;
+if (process.argv[process.argv.length-1] === "-i") {
+    process.stdout.write("You're in Interactive Mode.\n");
 
-app.listen(port, () => {
-    console.log("Listening to http://0.0.0.0:" + port)
-});
+    const reader = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    reader.on("line", (line) => {
+        executeLine(line);
+    });
+    reader.on("close", () => {
+        reader.close();
+        process.exit();
+    })
+} else {
+    const port = env.port || 4000;
+
+    app.listen(port, () => {
+        console.log("Listening to http://0.0.0.0:" + port);
+    });
+}
