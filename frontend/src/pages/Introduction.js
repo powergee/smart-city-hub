f74b65cc875f5f8ea3bd2a0e9a9c71e3b010a63d
Jeonghyeon Kim <powergee101@gmail.com>
2021-11-04 09:03:50 +0900

    Add researchers (인문사회연구소지원사업)

--- frontend/src/pages/Introduction.js
+++ frontend/src/pages/Introduction.js
@@ -1,9 +1,9 @@
-import { Paper, Grid } from '@material-ui/core';
+import { Paper, Grid, Button } from '@material-ui/core';
 import React, { useState, useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom';
 import { ContentContainer, ContentHeader, MarkdownViewer } from "../components"
 import jobs from "../shared/Jobs.json";
-import getResearchers from "../shared/Researchers.js";
+import { getGlobalRes, getProjectRes } from "../shared/Researchers.js";
 import goalDoc from "../docs/설립 배경 및 목적.md"
 import "./Introduction.scss"
 
@@ -70,6 +70,8 @@ function Goal() {
 }
 
 function Researchers() {
+    const [tab, setTab] = useState(0);
+
     const sections = [
         {
             title: "센터소개",
@@ -81,13 +83,50 @@ function Researchers() {
         }
     ];
 
-    const resInfo = getResearchers();
+    const globalRes = getGlobalRes();
+    const projectRes = getProjectRes();
 
     return (
         <ContentContainer currentPath={sections[1].link}>
             <ContentHeader sections={sections}>
-                {
-                    resInfo.map((res) => (
+                <div className="researchers-tab-container">
+                    <Button 
+                        size="large"
+                        variant="outlined"
+                        onClick={() => setTab(0)}
+                        disabled={0 === tab}
+                        color="primary"
+                    >국제도시 및 인프라 연구센터</Button>
+
+                    <Button
+                        size="large"
+                        variant="outlined"
+                        onClick={() => setTab(1)}
+                        disabled={1 === tab}
+                        color="primary"
+                    >인문사회연구소지원사업 참여 교수</Button>
+                </div>
+                <div style={{"display": (tab === 0 ? "block" : "none")}}>
+                    {globalRes.map((res) => (
+                        <Paper variant="outlined" className="researchers-paper">
+                            <Grid className="researchers-grid" container direction="row" alignItems='center'>
+                                <img alt="" src={res.image}></img>
+                                <Grid item>
+                                    <h2>{res.name}</h2>
+                                    <ul>
+                                        {
+                                            res.details.map((d) => (
+                                                <li>{d}</li>
+                                            ))
+                                        }
+                                    </ul>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    ))}
+                </div>
+                <div style={{"display": (tab === 1 ? "block" : "none")}}>
+                    {projectRes.map((res) => (
                         <Paper variant="outlined" className="researchers-paper">
                             <Grid className="researchers-grid" container direction="row">
                                 <img alt="" src={res.image}></img>
@@ -103,8 +142,8 @@ function Researchers() {
                                 </Grid>
                             </Grid>
                         </Paper>
-                    ))
-                }
+                    ))}
+                </div>
             </ContentHeader>
         </ContentContainer>
     )
--- frontend/src/shared/Researchers.js
+++ frontend/src/shared/Researchers.js
@@ -6,7 +6,15 @@ import resKJYong from "../images/res-KJYeong.jpg";
 import resPSC from "../images/res-PSC.jpg";
 import resYCH from "../images/res-YCH.jpg";
 
-export default function getResearchers() {
+import resProjCYS from "../images/res-project-CYS.png";
+import resProjKKI from "../images/res-project-KKI.jpg";
+import resProjLS from "../images/res-project-LS.png";
+import resProjPH from "../images/res-project-PH.png";
+import resProjPHM from "../images/res-project-PHM.jpg";
+import resProjPJ from "../images/res-project-PJ.jpg";
+import resProjSSB from "../images/res-project-SSB.jpg";
+
+function getGlobalRes() {
     return [
         {
             name: "염춘호",
@@ -126,3 +134,144 @@ export default function getResearchers() {
         },
     ];
 }
+
+function getProjectRes() {
+    return [
+        {
+            name: "염춘호",
+            image: resYCH,
+            details: [
+                "연구책임자",
+                "연구 총괄 및 스마트교통 기술 연구",
+                "서울시립대학교 국제도시과학대학원",
+                "글로벌건설학과 주임교수",
+                "서울시 동대문구 서울시립대로 163, 법학관 517호",
+                "전    화: 02-6490-5154",
+                "연락처: 010-2969-2504",
+                "Email : chunhoy7@uos.ac.kr"
+            ]
+        },
+        {
+            name: "최윤수",
+            image: resProjCYS,
+            details: [
+                "공동연구원 (일반공동)",
+                "측지 및 측량 분야 수출 기술 발굴",
+                "서울시립대학교 공간정보학과",
+                "서울시 동대문구 서울시립대로 163, 21세기관 509호",
+                "전    화: 02-6490-2887",
+                "Email : choiys@uos.ac.kr"
+            ]
+        },
+        {
+            name: "이신",
+            image: resProjLS,
+            details: [
+                "공동연구원 (일반공동)",
+                "스마트도시 관련 다학제 연계 구상",
+                "서울시립대학교 국제도시과학대학원",
+                "서울시 동대문구 서울시립대로 163, 법학관 514호",
+                "전    화: 02-6490-5151",
+                "Email : shinlee714@uos.ac.kr"
+            ]
+        },
+        {
+            name: "박준",
+            image: resProjPJ,
+            details: [
+                "공동연구원 (일반공동)",
+                "국외 스마트도시 도시 계획 분야 수요 조사 연구",
+                "서울시립대학교 국제도시과학대학원",
+                "서울시 동대문구 서울시립대로 163, 법학관 516호",
+                "전    화: 02-6490-5153",
+                "Email : joon.park@uos.ac.kr"
+            ]
+        },
+        {
+            name: "김광일",
+            image: resProjKKI,
+            details: [
+                "공동연구원 (일반공동)",
+                "인적 네트워크 구축 방안 연구",
+                "서울시립대학교 중국어문화학과",
+                "서울시 동대문구 서울시립대로 163, 인문학관 5212호",
+                "전    화: 02-6490-2594",
+                "Email : kikim@uos.ac.kr"
+            ]
+        },
+        {
+            name: "서승범",
+            image: resProjSSB,
+            details: [
+                "공동연구원 (일반공동)",
+                "국외 스마트환경 기술 수요 조사 연구",
+                "서울시립대학교 국제도시과학대학원",
+                "서울시 동대문구 서울시립대로 163, 법학관 510호",
+                "전    화: 02-6490-5135",
+                "Email : sbseo7@uos.ac.kr"
+            ]
+        },
+        {
+            name: "박현",
+            image: resProjPH,
+            details: [
+                "공동연구원 (일반공동)",
+                "대학원 내 과제 신설 및 특강, 세미나 개최 계획 추진",
+                "서울시립대학교 국제도시과학대학원",
+                "서울시 동대문구 서울시립대로 163, 법학관 513호",
+                "전    화: 02-6490-5146",
+                "Email : hpark@uos.ac.kr"
+            ]
+        },
+        {
+            name: "박혜민",
+            image: resProjPHM,
+            details: [
+                "공동연구원 (일반공동)",
+                "환경 분야 수출 기술 및 규제 발굴",
+                "서울시립대학교 국제도시과학대학원",
+                "서울시 동대문구 서울시립대로 163, 법학관 515호",
+                "전    화: 02-528-5152",
+                "Email : minpark@uos.ac.kr"
+            ]
+        },
+        {
+            name: "고주연",
+            image: resKJY,
+            details: [
+                "공동연구원 (전임연구인력)",
+                "국내외 스마트도시 정책 및 현황 분석",
+                "서울시립대학교 도시과학연구원 박사/연구교수",
+                "서울특별시 동대문구 서울시립대로 163, 21세기관 104-1호",
+                "전    화: 02-6490-5320",
+                "이메일: gojy@uos.ac.kr"
+            ]
+        },
+        {
+            name: "김대일",
+            image: resKDI,
+            details: [
+                "공동연구원 (전임연구인력)",
+                "스마트도시 기술 현황 조사 분석",
+                "공학박사/연구교수",
+                "서울시립대학교 국제도시 및 인프라연구센터[도시과학연구원]",
+                "서울특별시 동대문구 서울시립대로 163, 21세기관 104-1호",
+                "이메일:  kkim019@uos.ac.kr"
+            ]
+        },
+        {
+            name: "김동우",
+            image: resKDW,
+            details: [
+                "공동연구원 (전임연구인력)",
+                "스마트도시 기술 수출 플랫폼 개발",
+                "공학박사",
+                "서울시립대학교 국제도시 및 인프라연구센터[도시과학연구원]",
+                "서울특별시 동대문구 서울시립대로 163, 21세기관 104-1호",
+                "이메일:  dkim166@uos.ac.kr"
+            ]
+        },
+    ];
+}
+
+export { getGlobalRes, getProjectRes };
