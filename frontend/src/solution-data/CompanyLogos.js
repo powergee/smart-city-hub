964e400dcb46610606b8c630650c29775c19adb2
Lee-garam <lgr421@gmail.com>
2023-03-10 13:48:24 +0900

    imgs are updated. (solution, comp)

--- frontend/src/solution-data/CompanyLogos.js
+++ frontend/src/solution-data/CompanyLogos.js
@@ -10,6 +10,37 @@ import comp8 from "./logo-images/comp8.jpeg";
 import comp9 from "./logo-images/comp9.png";
 import comp10 from "./logo-images/comp10.jpeg";
 import comp11 from "./logo-images/comp11.jpeg";
+import comp12 from "./logo-images/comp12.png";
+import comp13 from "./logo-images/comp13.png";
+import comp14 from "./logo-images/comp14.jpeg";
+import comp15 from "./logo-images/comp15.gif";
+import comp16 from "./logo-images/comp16.png";
+import comp17 from "./logo-images/comp17.jpeg";
+import comp18 from "./logo-images/comp18.png";
+import comp19 from "./logo-images/comp19.png";
+import comp20 from "./logo-images/comp20.jpeg";
+import comp21 from "./logo-images/comp21.gif";
+import comp22 from "./logo-images/comp22.png";
+import comp23 from "./logo-images/comp23.png";
+import comp24 from "./logo-images/comp24.png";
+import comp25 from "./logo-images/comp25.png";
+import comp26 from "./logo-images/comp26.png";
+import comp27 from "./logo-images/comp27.png";
+import comp28 from "./logo-images/comp28.jpeg";
+import comp29 from "./logo-images/comp29.jpeg";
+import comp30 from "./logo-images/comp30.png";
+import comp31 from "./logo-images/comp31.png";
+import comp32 from "./logo-images/comp32.png";
+import comp33 from "./logo-images/comp33.png";
+import comp34 from "./logo-images/comp34.jpeg";
+import comp35 from "./logo-images/comp35.jpeg";
+import comp36 from "./logo-images/comp36.png";
+import comp37 from "./logo-images/comp37.jpeg";
+import comp38 from "./logo-images/comp38.png";
+import comp39 from "./logo-images/comp39.png";
+import comp40 from "./logo-images/comp40.png";
+import comp41 from "./logo-images/comp41.jpeg";
+import comp42 from "./logo-images/comp42.jpeg";
 
 export default [
   comp0,
@@ -24,4 +55,35 @@ export default [
   comp9,
   comp10,
   comp11,
+  comp12,
+  comp13,
+  comp14,
+  comp15,
+  comp16,
+  comp17,
+  comp18,
+  comp19,
+  comp20,
+  comp21,
+  comp22,
+  comp23,
+  comp24,
+  comp25,
+  comp26,
+  comp27,
+  comp28,
+  comp29,
+  comp30,
+  comp31,
+  comp32,
+  comp33,
+  comp34,
+  comp35,
+  comp36,
+  comp37,
+  comp38,
+  comp39,
+  comp40,
+  comp41,
+  comp42,
 ];
