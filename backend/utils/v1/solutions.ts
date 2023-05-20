f338c58f72db5420853260dc5dc0e80e6a6cfced
Seongho Lee <tjdgh2626@gmail.com>
2023-05-20 18:23:29 +0000

    Update CRUD of SolutionCompanyModel

--- backend/utils/v1/solutions.ts
+++ backend/utils/v1/solutions.ts
@@ -12,6 +12,68 @@ const router = new Router();
 router.use(BodyParser());
 
 /**
+ * GET /solutions/companies/
+ * 등록된 모든 회사 정보를 가져옵니다.
+ */
+router.get("/companies", async (ctx: Koa.Context) => {
+  const companies = await SolutionCompanyModel.find({});
+  ctx.body = companies;
+});
+
+router.get("/companies/:id", async (ctx: Koa.Context) => {
+  const id = ctx.params.id;
+  const company = await SolutionCompanyModel.findById(id);
+
+  if (!company) {
+    ctx.throw(404, "No document according to given id");
+  }
+
+  ctx.body = company;
+});
+
+/**
+ * POST /solutions/companies/
+ * 새 회사를 추가합니다.
+ * _id 필드가 제공되면 해당 Document를 수정합니다.
+ */
+router.post("/companies", async (ctx: Koa.Context) => {
+  const data = { ...ctx.request.body };
+
+  let company: SolutionCompany;
+
+  if (!data._id) {
+    // create new document
+    company = new SolutionCompanyModel(data);
+  } else {
+    // find and modify existing document
+    company = await SolutionCompanyModel.findById(data._id);
+
+    if (!company) {
+      ctx.throw(404, "No document according to given id");
+    }
+
+    // replace current fields with requested fields
+    Object.keys(data).forEach((key) => {
+      company.set(key, data[key]);
+    });
+  }
+
+  ctx.body = await company.save();
+});
+
+/**
+ * DELETE /companies/:id
+ * id에 해당하는 회사 Document를 삭제합니다.
+ * 해당 회사에 등록돼 있는 솔루션이 있는 경우, 해당 요청은 실패합니다.
+ */
+router.delete("/companies/:id", async (ctx: Koa.Context) => {
+  const id = ctx.params.id;
+  // TODO: 해당 회사에 솔루션이 등록돼 있는지 확인하고, 없는 경우에만 삭제 수행
+  const company = await SolutionCompanyModel.findByIdAndDelete(id);
+  ctx.body = company;
+});
+
+/**
  * GET /solutions/
  * 모든 솔루션 정보를 `detail` 필드를 제외하고 가져옵니다.
  *
@@ -95,29 +157,6 @@ router.delete("/:id", async (ctx: Koa.Context) => {
   const id = ctx.params.id;
   const solution = await SolutionModel.findByIdAndDelete(id);
   ctx.body = solution;
-});
-
-/**
- * GET /solutions/companies/
- * 등록된 모든 회사 정보를 가져옵니다.
- */
-router.get("/companies", async (ctx: Koa.Context) => {
-  const companies = await SolutionCompanyModel.find({});
-  ctx.body = companies;
-});
-
-router.post("/companies", async (ctx: Koa.Context) => {
-  const company: SolutionCompany = ctx.request?.body;
-
-  if (!company._id) {
-    // create new company document
-    const newCompany = new SolutionCompanyModel(company);
-    const res = await newCompany.save();
-
-    ctx.body = res;
-  } else {
-    ctx.throw(400, "_id field is given");
-  }
 });
 
 export default router;
