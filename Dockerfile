06ff8ce9457a485f3430a90208122cf252d8d715
Seongho Lee <tjdgh2626@gmail.com>
2023-05-24 17:45:12 +0000

    Implement docker environment

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:10 AS builder
+COPY ./frontend /frontend
+
+# build frontend
+WORKDIR /frontend
+RUN npm install
+RUN npm run build
+
+FROM node:12 AS product
+COPY ./backend /app
+COPY --from=builder /frontend /app/client
+
+# build backend
+WORKDIR /app
+RUN npm install
+
+RUN sed -i s/deb.debian.org/archive.debian.org/g /etc/apt/sources.list
+RUN sed -i 's|security.debian.org|archive.debian.org/|g' /etc/apt/sources.list
+RUN sed -i '/stretch-updates/d' /etc/apt/sources.list
+
+RUN apt update
+RUN apt install -y ghostscript graphicsmagick
+
+WORKDIR /app
+EXPOSE 4000
+CMD ["npx", "ts-node", "/app/index.ts"]
--- backend/env.ts
+++ backend/env.ts
@@ -1,30 +1,29 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 
 interface IEnv {
-    port: string,
-    mongoURI: string,
-    accessTokenKey: string,
-    filesDirectory: string
+  port: string;
+  mongoURI: string;
+  accessTokenKey: string;
+  filesDirectory: string;
 }
 
-if (process.env.PORT === undefined)
-    throw new Error("PORT is not set.");
+if (process.env.PORT === undefined) process.env.PORT = "4000";
 
 if (process.env.MONGO_URI === undefined)
-    throw new Error("MONGO_URI is not set.");
+  throw new Error("MONGO_URI is not set.");
 
 if (process.env.ACCESS_SECRET_KEY === undefined)
-    throw new Error("ACCESS_SECRET_KEY is not set.");
+  throw new Error("ACCESS_SECRET_KEY is not set.");
 
 if (process.env.FILES_DIRECTORY === undefined)
-    throw new Error("FILES_DIRECTORY is not set.");
+  throw new Error("FILES_DIRECTORY is not set.");
 
-const env:IEnv = {
-    port: process.env.PORT,
-    mongoURI: process.env.MONGO_URI,
-    accessTokenKey: process.env.ACCESS_SECRET_KEY,
-    filesDirectory: process.env.FILES_DIRECTORY
-}
+const env: IEnv = {
+  port: process.env.PORT,
+  mongoURI: process.env.MONGO_URI,
+  accessTokenKey: process.env.ACCESS_SECRET_KEY,
+  filesDirectory: process.env.FILES_DIRECTORY,
+};
 
-export default env
+export default env;
