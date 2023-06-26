7b81a6b1bfc76b8e639d96a4fa04373b1390948e
Seongho Lee <tjdgh2626@gmail.com>
2023-06-26 05:32:39 +0000

    Add scroll to top function

--- frontend/src/shared/App.js
+++ frontend/src/shared/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import { Main } from '../pages';
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Main } from "../pages";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/" component={Main}></Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" component={Main}></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/shared/ScrollToTop.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+// https://stackoverflow.com/questions/36904185/react-router-scroll-to-top-on-every-transition
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
