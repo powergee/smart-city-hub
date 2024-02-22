4b1043774e3f75af6cfb09f4200a613d8ed8448c
Seongho Lee <tjdgh2626@gmail.com>
2024-02-22 02:08:58 +0000

    Add getSuperCategoryNameAll method in SolutionRepository

--- web/core/repository.ts
+++ web/core/repository.ts
@@ -1,5 +1,23 @@
 import { AseanBannerItem, Locale } from "core/model";
 
+type LocaleRepositoryMapper<R> = { [key in Locale]: R };
+
+export class LocaleFacade<R> {
+  private localeInstances: LocaleRepositoryMapper<R>;
+
+  constructor(localeInstances: LocaleRepositoryMapper<R>) {
+    this.localeInstances = localeInstances;
+  }
+
+  pickLocale(lang: Locale): R {
+    return this.localeInstances[lang];
+  }
+}
+
 export interface AseanBannerRepository {
   getItemAll: (lang: Locale) => AseanBannerItem[];
+}
+
+export interface SolutionRepository {
+  getSuperCategoryNameAll: () => Promise<{ id: string; name: string }[]>;
 }
--- web/repository/index.ts
+++ web/repository/index.ts
@@ -1,13 +1,23 @@
 /* interfaces */
-import { AseanBannerRepository } from "core/repository";
+import {
+  AseanBannerRepository,
+  LocaleFacade,
+  SolutionRepository,
+} from "core/repository";
 
 /* repositories */
 import AseanBannerTextRepo from "repository/asean/asean-banner-text";
+import SolutionTextRepo from "repository/solution/solution-text";
 
 /* dependency injection */
 const aseanBanner: AseanBannerRepository = new AseanBannerTextRepo();
+const solution: LocaleFacade<SolutionRepository> = new LocaleFacade({
+  ko: new SolutionTextRepo("ko"),
+  en: new SolutionTextRepo("en"),
+});
 
 /* export */
 export const repo = {
   aseanBanner,
+  solution,
 };
