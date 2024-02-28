0a5c52356080c2d25fa5aa5d1285d579f98025eb
Seongho Lee <tjdgh2626@gmail.com>
2024-02-28 14:54:35 +0000

    Update homepage(Add research and project banner)

--- web/app/[lang]/page.tsx
+++ web/app/[lang]/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import { Locale } from "core/model";
 import { repo } from "repository";
 import { initTranslation } from "@locales";
@@ -6,8 +8,10 @@ import Container from "@components/container";
 import { FormalHeader2 } from "@components/typography";
 import AseanBanner from "@components/home/asean-banner";
 import SolutionBanner from "@components/home/solution-banner";
+import Carousel from "@components/carousel";
 
 import { getSolutionCoverById } from "@resources/images/solution-covers";
+import urbanCityLandscapeImg from "@resources/images/urban-city-landscape.jpg";
 
 export default async function Home(props: { params: { lang: string } }) {
   const lang = props.params.lang as Locale;
@@ -15,7 +19,7 @@ export default async function Home(props: { params: { lang: string } }) {
 
   return (
     <>
-      <div className="bg-global-gray-light pt-4 pb-6 border-b">
+      <div className="bg-global-gray-light pt-4 pb-8 border-b">
         <Container>
           <FormalHeader2>{t("asean-banner-header")}</FormalHeader2>
         </Container>
@@ -43,9 +47,54 @@ export default async function Home(props: { params: { lang: string } }) {
           />
         </Container>
       </div>
-      <Container className="mt-6">
-        <FormalHeader2 className="mt-8">총괄연구 & 사업</FormalHeader2>
-        <FormalHeader2 className="mt-8">연구센터 소개</FormalHeader2>
+      <div className="relative border-b">
+        <Image
+          className="absolute -z-10 w-full h-full object-center object-cover"
+          src={urbanCityLandscapeImg}
+          alt="Urban City Landscape"
+          width={720}
+        />
+        <div className="backdrop-blur-sm bg-uos-gray/60 py-8">
+          <Container className="text-white">
+            <div className="grid gap-8 grid-cols-1 md:grid-cols-2">
+              {/* 총괄 연구 & 사업 배너 */}
+              <div className="min-h-72">
+                <FormalHeader2
+                  className="drop-shadow-md"
+                  style={{ borderColor: "#fff" }}
+                >
+                  {t("research-project-header")}
+                </FormalHeader2>
+                <Carousel className="h-full" recursive interval={5000}>
+                  {(
+                    await repo.projectRecord.pickLocale(lang).getItemList(true)
+                  ).map((item, idx) => (
+                    <div
+                      className="px-16 flex flex-col justify-center text-center w-full h-full drop-shadow-md break-keep"
+                      key={idx}
+                    >
+                      <div className="font-medium text-3xl">{item.title}</div>
+                      <div className="font-light text-xl mt-4">
+                        {item.client} [{item.year[0]}~{item.year[1]}]
+                      </div>
+                    </div>
+                  ))}
+                </Carousel>
+              </div>
+              {/* 연구센터 소개 배너 */}
+              <div className="min-h-72">
+                <FormalHeader2
+                  className="drop-shadow-md"
+                  style={{ borderColor: "#fff" }}
+                >
+                  {t("research-center-introduction")}
+                </FormalHeader2>
+              </div>
+            </div>
+          </Container>
+        </div>
+      </div>
+      <Container>
         <FormalHeader2 className="mt-8">최신 Update</FormalHeader2>
         <FormalHeader2 className="mt-8">최신 Issue Paper</FormalHeader2>
         <FormalHeader2 className="mt-8">
--- web/app/_components/typography.tsx
+++ web/app/_components/typography.tsx
@@ -3,12 +3,14 @@ import React from "react";
 export function FormalHeader2(props: {
   children: React.ReactNode;
   className?: string;
+  style?: React.CSSProperties;
 }) {
   return (
     <h2
       className={`border-l-4 border-uos-blue font-medium text-lg pl-3 ${
         props.className ?? ""
       }`}
+      style={props.style}
     >
       {props.children}
     </h2>
