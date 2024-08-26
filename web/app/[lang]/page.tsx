84ed23680fac878cb548e61e2f3394a9ae7535e9
Seongho Lee <shythm@outlook.com>
2024-08-26 17:01:42 +0000

    update: hub directory structure

--- web/app/[lang]/page.tsx
+++ web/app/[lang]/page.tsx
@@ -16,7 +16,7 @@ import { getSolutionCoverByIndex } from "@resources/images/solution-covers";
 import { quickMenuIcons } from "@resources/images/quick-menu-icons";
 import urbanCityLandscapeImg from "@resources/images/urban-city-landscape.jpg";
 
-import { getSolutionCategoryAll } from "./(solution)/hub/[cateid]/categories";
+import { getSolutionCategoryAll } from "./hub/[cateid]/categories";
 
 export default async function Home(props: { params: { lang: string } }) {
   const lang = props.params.lang as Locale;
