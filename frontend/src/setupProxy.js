0b27db5361e8f08698873c604e3f6402e0c84286
Seongho Lee <shythm@outlook.com>
2024-08-27 16:56:41 +0000

    remove: legacy frontend

--- frontend/src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-module.exports = function (app) {
-  app.use(createProxyMiddleware("/v1", { target: process.env.BACKEND_API_URL }));
-};
